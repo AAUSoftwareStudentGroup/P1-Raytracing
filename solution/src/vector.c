0a56c8e74cbcd45f7ed289e246b660d7d6856294
Kristian Træholt Sørensen <kts92@hotmail.com>
2015-11-12 11:17:30 +0100

    fjernet comments

--- solution/src/vector.c
+++ solution/src/vector.c
@@ -25,9 +25,9 @@ Vector vector_normalize(Vector v) {
 }
 
 double vector_angle_between(Vector v1, Vector v2) {
-  return acos(vector_dot(v1, v2)/(vector_norm(v1) * vector_norm(v2))); // Hvorfor ikke bare vector_norm(v)?
+  return acos(vector_dot(v1, v2)/(vector_norm(v1) * vector_norm(v2))); 
 }
 
 Vector vector_cross(Vector v1, Vector v2) {
-  return (Vector){(v1.y * v2.z) - (v1.z * v2.y), (v1.z * v2.x) - (v1.x * v2.x), (v1.x * v2.y) - (v1.y * v1.x)}; // Går ud fra det er denne fremgangsmåde?
+  return (Vector){(v1.y * v2.z) - (v1.z * v2.y), (v1.z * v2.x) - (v1.x * v2.x), (v1.x * v2.y) - (v1.y * v1.x)}; 
 }
