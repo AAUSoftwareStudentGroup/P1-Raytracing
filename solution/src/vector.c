1c5a3afd6c2bb1a84e059704612ad174836df987
Anton Christensen <anton.christensen9700@gmail.com>
2015-11-12 08:43:43 +0100

    Retter småting i vektor typen

--- solution/src/vector.c
+++ solution/src/vector.c
@@ -1,33 +1,33 @@
 #include "vector.h"
 
 Vector vector_add(Vector v1, Vector v2) {
-  return (Vector){v1.x + v2.x, v1.y + v2.y, v1.z + v2.z;}
+  return (Vector){v1.x + v2.x, v1.y + v2.y, v1.z + v2.z};
 }
 
 Vector vector_subtract(Vector v1, Vector v2) {
-  return v1.x - v2-x, v2.y - v1.y, v1.z - v2.z;}
+  return (Vector){v1.x - v2.x, v1.y - v2.y, v1.z - v2.z};
 }
 
 Vector vector_scale (Vector v, double s) {
-  return v.x * s, v.y * s, v.z * s;}
+  return (Vector){v.x * s, v.y * s, v.z * s};
 }
 
 double vector_dot (Vector v1, Vector v2) {
-  return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;}
+  return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
 }
 
 double vector_norm(Vector v) {
-  return sqrt(vector_dot(v1, v2));}
+  return sqrt(vector_dot(v1, v2));
 }
 
 Vector vector_normalize(Vector v) {
-  return vector_scale(1/vector_norm(v), v)}
+  return vector_scale(1/vector_norm(v), v);
 }
 
 double vector_angle_between(Vector v1, Vector v2) {
-  return acos(vector_dot(v1, v2)/vector_norm(v1) * vector_norm(v2));} // Hvorfor ikke bare vector_norm(v)?
+  return acos(vector_dot(v1, v2)/(vector_norm(v1) * vector_norm(v2)));} // Hvorfor ikke bare vector_norm(v)?
 }
 
 Vector vector_cross(Vector v1, Vector v2) {
-  return ((v1.y * v2.z) - (v1.z * v2.y), (v1.z * v2.x) - (v1.x * v2.x), (v1.x * v2.y) - (v1.y * v1.x));} // Går ud fra det er denne fremgangsmåde?
+  return (Vector){(v1.y * v2.z) - (v1.z * v2.y), (v1.z * v2.x) - (v1.x * v2.x), (v1.x * v2.y) - (v1.y * v1.x)}; // Går ud fra det er denne fremgangsmåde?
 }
